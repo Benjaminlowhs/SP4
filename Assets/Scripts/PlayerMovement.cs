f5ffe6d9ca7997e8f7afdaf1eb68354376712c7e
hong-jun123 <99625310+hong-jun123@users.noreply.github.com>
2023-02-15 15:41:29 +0800

    sprint

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -33,7 +33,11 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move;
+        if (Input.GetKey(KeyCode.LeftShift))
+            move = transform.right * (x * 2f) + transform.forward * (z * 2f);
+        else
+            move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
