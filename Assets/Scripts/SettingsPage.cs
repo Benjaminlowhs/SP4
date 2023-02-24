36fca72217099dbd5da7aec17bd2e576585a6846
Benjaminlowhs <88994519+Benjaminlowhs@users.noreply.github.com>
2023-02-24 10:44:05 +0800

    Update mainmenu scaling, update settings page
    removed unnecessary settings stuff and reduce to just sfx, music and graphics

--- Assets/Scripts/SettingsPage.cs
+++ Assets/Scripts/SettingsPage.cs
@@ -11,35 +11,10 @@ public class SettingsPage : MonoBehaviour
 
     public AudioMixer audioMixer;
 
-    public TMP_Dropdown resolutionDropdown;
-
-    Resolution[] resolutions;
 
     void Start()
     {
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        
     }
     public void SetVolume (float volume)
     {
@@ -48,13 +23,8 @@ public class SettingsPage : MonoBehaviour
 
     public void SetQuality (int qualityIndex)
     {
+        qualityIndex += 3;
         QualitySettings.SetQualityLevel(qualityIndex);
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
 }
